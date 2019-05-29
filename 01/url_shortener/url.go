b929ebfbfcc82155ef1ef0ea71f20c8b1f7bf980
Denis Balabanov <balabanov.ds@gmail.com>
2019-05-29 13:18:42 +0300

    hw01 url shortener compleeted

--- 01/url_shortener/url.go
+++ 01/url_shortener/url.go
@@ -1,8 +1,13 @@
 package url_shortener
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 	"log"
 	"net/url"
+	"strconv"
+	"strings"
 )
 
 // Shortener interface
@@ -16,19 +21,62 @@ type URL string
 
 var data map[string]string
 
+func init() {
+	data = make(map[string]string)
+}
+
 // Shorten url, and stores it in map
-func (u *URL) Shorten(s string) string {
-	url, err := url.Parse(s)
+func (*URL) Shorten(s string) string {
+	u, err := parse(s)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	path := url.EscapedPath()
+	path := hash(u.EscapedPath())
+	hostname := u.Hostname()
 
-	return "tets"
+	shortURL := fmt.Sprintf("%s/%s", hostname, path)
+
+	data[shortURL] = s
+
+	return shortURL
 }
 
-// Resolve url from short to normal
-func (u *URL) Resolve(s string) string {
+// Resolve url from short to normal or empty if not found
+func (*URL) Resolve(s string) string {
+	if res, ok := data[s]; ok {
+		return res
+	}
 	return ""
+}
+
+func hash(str string) string {
+	hash := getMD5(str)
+	var sum uint64
+	for _, b := range []byte(hash) {
+		sum += uint64(b)
+	}
+	return strconv.FormatUint(sum+1e5, 16)
+}
+
+func parse(s string) (*url.URL, error) {
+	addHttp(&s)
+	u, err := url.Parse(s)
+	if err != nil {
+		return nil, err
+	}
+
+	return u, nil
+}
+
+func addHttp(str *string) {
+	if !strings.Contains(*str, "http") {
+		*str = "http://" + *str
+	}
+}
+
+func getMD5(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
 }
--- 01/url_shortener/url_test.go
+++ 01/url_shortener/url_test.go
@@ -1,15 +1,39 @@
 package url_shortener
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestURLWithoutHttpPrefix(t *testing.T) {
-	url := "otus.ru/link"
+	url := "otus.ru/link go"
 	var u URL
 
 	short := u.Shorten(url)
+	long := u.Resolve(short)
 
-	fmt.Println(short)
+	if long != url {
+		t.Errorf("expected %v, got %v", url, long)
+	}
+}
+
+func TestURLWithHttpPrefix(t *testing.T) {
+	url := "https://otus.ru/link go"
+	var u URL
+
+	short := u.Shorten(url)
+	long := u.Resolve(short)
+
+	if long != url {
+		t.Errorf("expected %v, got %v", url, long)
+	}
+}
+
+func TestNotFoundURL(t *testing.T) {
+	var u URL
+
+	long := u.Resolve("some.ru/unknown")
+
+	if long != "" {
+		t.Error("expected empty string")
+	}
 }
