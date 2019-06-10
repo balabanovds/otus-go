2f7a8fad8d13605c24f7432e90441731e05d4ae0
Denis Balabanov <balabanov.ds@gmail.com>
2019-06-10 10:58:43 +0300

    hw03 done

--- word_counter.go
+++ word_counter.go
@@ -1,45 +1,75 @@
 package words
 
 import (
-	"fmt"
+	"regexp"
+	"sort"
 	"strings"
 )
 
-type word struct {
+type node struct {
 	count int
-	word string 
+	word  string
 }
 
-type text struct {
-	data map[string]int
-	rating []word
+type sortByCounter []node
+
+func (a sortByCounter) Len() int {
+	return len(a)
 }
 
-func newText() *text {
-	return &text{
-		data: make(map[string]int),
-		rating: []word{},
+func (a sortByCounter) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a sortByCounter) Less(i, j int) bool {
+	if a[i].count == a[j].count {
+		return a[i].word < a[j].word
 	}
+	return a[i].count > a[j].count
 }
 
-func (t *text) rate() {
-	res := []word{}
-	for word, count := range t.data {
-		for i, w := range t.rating {
-			if w.count < count {
-				res = in
-			}
-		}
-	}
-	t.rating = res
+type parser struct {
+	data   map[string]int
+	rating sortByCounter
 }
 
-// Count words in straing and returns first 10 more frequent
-func Count(str string) {
+func newTextParser(str string) *parser {
+	re := regexp.MustCompile(`\.|,`)
+	str = re.ReplaceAllString(str, "")
 	chunks := strings.Split(str, " ")
-	t := newText()
+	t := &parser{
+		data: make(map[string]int),
+	}
 	for _, c := range chunks {
 		t.data[c]++
 	}
-	fmt.Printf("%+v\n", t)
+
+	return t
+}
+
+func (t *parser) rate() {
+	t.rating = []node{}
+
+	for word, count := range t.data {
+		t.rating = append(t.rating, node{count, word})
+	}
+
+	sort.Sort(t.rating)
+}
+
+// Count words in straing and returns first 10 more frequent
+func Count(str string) []string {
+
+	t := newTextParser(str)
+	t.rate()
+
+	end := 10
+	if len(t.rating) < end {
+		end = len(t.rating)
+	}
+	res := make([]string, end)
+	for i, n := range t.rating[:end] {
+		res[i] = n.word
+	}
+	return res
 }
--- word_counter_test.go
+++ word_counter_test.go
@@ -1,10 +1,22 @@
 package words
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestCounter(t *testing.T) {
-	text := `a a a b b b b b b c c c c`
-	Count(text)
+	text := `Duis ea eu nisi proident eu cillum sint ad in ea minim excepteur. Consequat cillum in ullamco dolore deserunt irure laborum aute exercitation. Et culpa enim et laborum non enim laborum. Nisi ut aliquip laboris consectetur magna magna ullamco. Officia consectetur laboris ad irure ut non eiusmod. Exercitation exercitation duis occaecat commodo sit aute dolore sit eu cupidatat cupidatat. Deserunt labore nulla culpa nisi excepteur occaecat amet magna.
+Adipisicing irure officia in irure mollit ex culpa est nulla deserunt aliqua tempor. Do dolore voluptate duis cillum consectetur ad sint aute voluptate mollit consectetur ullamco tempor cillum. Proident fugiat nulla adipisicing aliqua cupidatat sit.
+Non velit enim sit in voluptate laboris officia cillum qui. Non fugiat magna do eiusmod reprehenderit. Nostrud aliquip voluptate cillum ex. Dolor ea irure ullamco pariatur reprehenderit sint excepteur.
+Cupidatat nisi mollit laborum aliquip pariatur laborum veniam tempor proident officia ad duis voluptate. Eu reprehenderit eu enim excepteur dolor officia ipsum sint do elit id consectetur. Aliqua pariatur incididunt in esse qui mollit. Nostrud aliqua ex excepteur dolor sunt esse esse anim culpa mollit labore culpa. Deserunt non ipsum commodo amet qui do officia enim.
+Magna ex consectetur eiusmod ea proident veniam irure labore. Cupidatat occaecat laboris id magna non elit. Consectetur nulla fugiat Lorem ex dolor nostrud sit voluptate et ullamco deserunt adipisicing. Culpa laborum ad laboris Lorem incididunt ad ex eiusmod ex. Elit officia consequat eu est laborum aliqua consequat quis. Cillum ad anim irure in reprehenderit mollit laborum magna. Ad laborum ea duis id voluptate anim qui adipisicing Lorem proident.
+Amet nulla consectetur fugiat aute et proident qui ad officia eiusmod nostrud qui id tempor. Et amet ex nostrud aliqua. Dolore amet mollit ipsum in nisi velit ipsum consectetur sit amet laboris cupidatat voluptate commodo.
+Commodo labore consequat quis duis reprehenderit dolore nulla cillum nisi incididunt ex. Dolor cillum elit quis id irure enim anim laboris adipisicing do. Nostrud quis irure et fugiat quis occaecat tempor esse veniam elit ex.
+Eu eiusmod velit quis proident Lorem consectetur consectetur quis commodo aliqua non nulla duis. Est dolor voluptate ad ullamco reprehenderit officia sunt esse exercitation dolore minim Lorem minim laboris. Enim nostrud nulla proident reprehenderit adipisicing sint voluptate irure veniam pariatur aute.`
+	expected := []string{"consectetur", "irure", "voluptate", "ad", "ex", "laborum", "cillum", "laboris", "nulla", "officia"}
+	got := Count(text)
+	if !reflect.DeepEqual(expected, got) {
+		t.Errorf("\nexpected:\t%+v\nreceived:\t%+v", expected, got)
+	}
 }
