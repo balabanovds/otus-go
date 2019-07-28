b15b67d3c151941ca039789ed2f1858189618a0b
Denis Balabanov <balabanov.ds@gmail.com>
2019-07-28 07:57:01 +0300

    fixed code review

--- pool.go
+++ pool.go
@@ -31,14 +31,17 @@ func Exec(jobs []fn, poolSize int, maxErrors int) {
 		go func(i int) {
 			// we subscribe on channel of jobs
 			for j := range ch {
-				log.Printf("worker %d, got job %d\n", i, j.num)
-				// if job return error we decrease counter
-				if j.fn() != nil {
-					c.Lock()
-					c.counter--
-					log.Printf("worker %d, job %d got error, counter is %d\n", i, j.num, c.counter)
-					c.Unlock()
+				c.Lock()
+				if c.counter > 0 {
+					log.Printf("worker %d, got job %d\n", i, j.num)
+					// if job return error we decrease counter
+					if j.fn() != nil {
+						c.counter--
+						log.Printf("worker %d, job %d got error, counter is %d\n", i, j.num, c.counter)
+					}
 				}
+				c.Unlock()
+
 			}
 			log.Printf("worker %d stopped\n", i)
 			wg.Done()
@@ -48,8 +51,8 @@ func Exec(jobs []fn, poolSize int, maxErrors int) {
 	// we are sending jobs to pool, and check counter
 	// if max errors reached, we stop
 	var i int
-	for i < len(jobs) {
-		ch <- job{num: i, fn: jobs[i]}
+	for _, j := range jobs {
+		ch <- job{num: i, fn: j}
 		log.Printf("manager sent job %d to pool\n", i)
 		i++
 		c.Lock()
